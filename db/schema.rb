b02c177eebca1c4f297127f2858400083cbd2131
fksdud456 <fksdud456@gmail.com>
2018-07-18 17:30:12 +0900

    Set rails mailer, auth email service

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180710073211) do
+ActiveRecord::Schema.define(version: 20180718064616) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
@@ -59,6 +59,10 @@ ActiveRecord::Schema.define(version: 20180710073211) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "profile_img"
